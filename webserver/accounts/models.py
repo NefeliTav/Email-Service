7ca5d198c89886c00fb9221238bd0e8daed26e19
Nikita Larichev <niklarichev@gmail.com>
2021-07-17 22:33:53 +0200

    Nginx reverse proxy and gunicorn for webserver

--- webserver/accounts/models.py
+++ webserver/accounts/models.py
@@ -2,12 +2,10 @@ import datetime
 from django.db import models
 
 class Account(models.Model):
-    user_id = models.CharField(max_length=30)
+    user_id = models.CharField(max_length=30, default="")
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=40, unique=True)
     phone = models.CharField(max_length=20)
     date_of_birth = models.DateField(default=datetime.date.today)
     password = models.CharField(max_length=20)
-
-
