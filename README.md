8638bd39e0a59d7785e3e6ccc22b68252a34de44
Nikita Larichev <niklarichev@gmail.com>
2021-07-19 18:30:19 +0200

    Locust stress test

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Webserver, antispam, emaildb, userdb
 ```
 docker-compose up
 ```
+
+## Stress-test
+
+Run
+```
+locust -f webserver/locust_test.py
+```
+and then open [web-interface](http://127.0.0.1:8089/). Default port is 8001.
---
+++ webserver/locust_test.py
@@ -0,0 +1,56 @@
+from locust import HttpUser, between, task
+
+import json
+import time
+import random
+
+RECEIVER = "xaxa@email.com"
+PASSWORD = "Abba1234"
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 2)
+
+    def on_start(self):
+        #self.login()
+        self.signup()
+
+    def signup(self):
+        email = str(time.time() * 1000) + str(random.randint(0, 1000000000))
+
+        req = {"first_name" : "Vladimir",
+               "last_name"  : "Navalny",
+               "email"      : email,
+               "date_of_birth" : "2000-02-02",
+               "password"   : PASSWORD,
+               "confirm"    : PASSWORD}
+        req = json.dumps(req)
+
+        self.client.post("/auth/signup/", req)
+
+    def login(self):
+        response = self.client.get('/auth/login/')
+        csrftoken = response.cookies['csrftoken']
+
+        req = {"email"    : "xaxa",
+               "password" : PASSWORD}
+        req = json.dumps(req)
+
+        self.client.post("/auth/login/", req)
+
+    @task(3)
+    def send_mail(self):
+        req = {"receiver" : RECEIVER,
+               "subject"  : "Report",
+               "content"  : "We have several options"}
+        req = json.dumps(req)
+
+        self.client.post('/send/', req)
+
+    @task
+    def send_spam(self):
+        req = {"receiver" : RECEIVER,
+                "subject"  : "XXX porn",
+                "content"  : "casino and porn"}
+        req = json.dumps(req)
+
+        self.client.post('/send/', req)
